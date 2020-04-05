8f1aa1745ff4ff9a73f2b63937a249fd0e41e5f5
Grant Dobbe <grant@dobbe.us>
2020-04-04 21:47:38 -0700

    - added test suite - added empty setup file

---
+++ tests/dkanbot_tests.py
@@ -0,0 +1,15 @@
+import os, unittest
+from dkan.client import DatasetAPI
+
+# test internet archive submit
+def test_archive_submit():
+    pass
+
+# test get node_id
+def test_node_id():
+    pass
+
+# test posting a resource to a dataset
+def test_resource_attach():
+    pass
+    
