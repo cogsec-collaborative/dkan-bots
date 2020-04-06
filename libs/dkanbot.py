49c200c5cf53f3a023a6fc7d7602f12f96bc6987
Grant Dobbe <grant@dobbe.us>
2020-04-06 15:15:17 -0700

    - added a function for retrieving resource based on title - renamed get_nodeid to get_dataset_nodeid - made useful unit tests

--- libs/dkanbot.py
+++ libs/dkanbot.py
@@ -1,14 +1,20 @@
 import os
 import requests
-import json
+import yaml
 import validators
 from dkan.client import DatasetAPI
 
-# set these as environment variables for now
-# TODO add a config file template
-uri = os.environ.get('DKAN_URI', False)
-user = os.environ.get('DKAN_USER', 'admin')
-password = os.environ.get('DKAN_PASSWORD', 'admin')
+
+with open(".config", "r") as configfile:
+    config = yaml.load(configfile, Loader=yaml.FullLoader)
+
+# set up an api session
+def create_api_session():
+    try:
+        api = DatasetAPI(config['dkan']['uri'], config['dkan']['user'], config['dkan']['password'])
+        return api
+    except Exception as e:
+        return e
 
 # toss url into internet archive
 def upload_to_archive(source_url):
@@ -25,9 +31,8 @@ def upload_to_archive(source_url):
     return asset_url
 
 # call DKAN to get the node id from the list of datasets
-def get_nodeid(title):
-    if uri:
-        api = DatasetAPI(uri, user, password)
+def get_dataset_nodeid(title):
+    api = create_api_session()
 
     # set up the query params
     params = {
@@ -36,48 +41,61 @@ def get_nodeid(title):
     }
     # make the api request and turn it into something usable
     r = api.node(params=params)
-    result = json.loads(r.content)
+    result = r.json()
 
     # if the results aren't bonkers, return them
-    assert (len(result) == 1)
+    return result[0]['nid']
+
+# call DKAN to get the node id from the list of resources
+def get_resource_nodeid(title):
+    api = create_api_session()
+
+    # set up the query params
+    params = {
+        'parameters[type]'  : 'resource',
+        'parameters[title]' : title
+    }
+    # make the api request and turn it into something usable
+    r = api.node(params=params)
+    result = r.json()
+
+    # if the results aren't bonkers, return them
     return result[0]['nid']
 
 # add internet archive url to defined dkan dataset
 def add_resource_to_dataset(ia_uri, trigger_source, dataset_nid=1):
-    if uri:
-
-        api = DatasetAPI(uri, user, password, False)
-        # take the back-end of the IA uri so we don't have to import it separately
-        source_uri = ia_uri[43:]
-        if (validators.url(source_uri) == True):
-            data = {
-                'title': '[dkanbot] Link to ' + source_uri,
-                'type': 'resource',
-                'body' : {
-                    'und': {
-                        '0': {
-                            'value': 'Data added by dkanbot from ' + str(trigger_source),
-                        }
+    api = create_api_session()
+   # take the back-end of the IA uri so we don't have to import it separately
+    source_uri = ia_uri[43:]
+    if (validators.url(source_uri) == True):
+        data = {
+            'title': '[dkanbot] Link to ' + source_uri,
+            'type': 'resource',
+            'body' : {
+                'und': {
+                    '0': {
+                        'value': 'Data added by dkanbot from ' + str(trigger_source),
                     }
-                },
-                'field_dataset_ref': {
-                    'und': {
-                        '0': {
-                            'target_id': dataset_nid,
-                        }
+                }
+            },
+            'field_dataset_ref': {
+                'und': {
+                    '0': {
+                        'target_id': dataset_nid,
                     }
-                },
-                'field_link_api': {
-                    'und': {
-                        '0': {
-                            'url': ia_uri
-                        }
+                }
+            },
+            'field_link_api': {
+                'und': {
+                    '0': {
+                        'url': ia_uri
                     }
                 }
             }
+        }
 
-            r = api.node('create', data=data)
-            return json.loads(r.text)
-        else:
-            print("invalid source URI")
-            return 1
+        r = api.node('create', data=data)
+        return r.json()
+    else:
+        print("invalid source URI")
+        return 1
--- tests/dkanbot_tests.py
+++ tests/dkanbot_tests.py
@@ -1,51 +1,56 @@
 import unittest
+import json
 from libs import dkanbot
 from dkan.client import DatasetAPI
 
 class TestDKANBot(unittest.TestCase):
-
     # set up a dummy resource
     def setUp(self):
-        uri = os.environ.get('DKAN_URI', False)
-        user = os.environ.get('DKAN_USER', 'admin')
-        password = os.environ.get('DKAN_PASSWORD', 'admin')
+        self.api = dkanbot.create_api_session()
+        data = {
+            'title': 'Test Suite Dataset',
+            'type': 'dataset'
+        }
+        dataset = self.api.node('create', data=data)
+        result = dataset.json()
+        self.dataset_nid = result['nid']
+        self.dataset_uri = result['uri']
+        self.test_uri = 'https://www.example.com'
+        self.trigger = "Dkanbot unit test suite"
+        self.cleanup = [self.dataset_nid]
 
-        if uri:
-            api = DatasetAPI(uri, user, password)
-
-            data = {
-                'title': 'Test Suite Dataset',
-                'type': 'dataset'
-            }
-            dataset = api.node('create', data=data)
-            result = 
+    # clean up
+    def tearDown(self):
+        self.cleanup = list(set(self.cleanup))
+        for node in self.cleanup:
+            delete = self.api.node('delete', node_id=node)
+            self.assertEqual(delete.status_code, 200)
+        self.cleanup = []
 
     # test internet archive submit
     def test_archive_submit(self):
-        test_uri = 'https://www.example.com'
-        result = dkanbot.upload_to_archive(test_uri)
+        result = dkanbot.upload_to_archive(self.test_uri)
         self.assertRegex(result, 'https:\/\/web\.archive\.org\/web\/[\d]+\/https:\/\/www\.example\.com$')
 
     # test get node_id
-    def test_node_id(self):
-        test_nid = '1'
-        result = dkanbot.get_nodeid('Example dataset')
-        self.assertEqual(test_nid, result)
+    def test_dataset_node_id(self):
+        result = dkanbot.get_dataset_nodeid('Test Suite Dataset')
+        self.assertEqual(self.dataset_nid, result)
 
     # test posting a resource to a dataset
     def test_resource_attach(self):
-        # get a uri from IA
-        uri_stub = 'https://www.example.com'
-        test_uri = dkanbot.upload_to_archive(uri_stub)
-        test_nid = '1'
-        test_trigger = 'dkanbot unit test'
+        ia_uri = dkanbot.upload_to_archive(self.test_uri)
         # test posting it
-        result = dkanbot.add_resource_to_dataset(test_uri, test_trigger, test_nid)
+        result = dkanbot.add_resource_to_dataset(ia_uri, self.trigger, self.dataset_nid)
+        self.cleanup.append(result['nid'])
         # first, make sure it's not an error return
         self.assertNotEqual(result, 1)
         # now, make sure it matches the node_id we got back
-        self.assertEqual(dkanbot.get_nodeid('[dkanbot] Link to ' + uri_stub, result['nid'])
+        self.assertEqual(dkanbot.get_resource_nodeid('[dkanbot] Link to ' + self.test_uri), result['nid'])
+        # now, make sure the URL matches
+        r = self.api.node('retrieve', node_id=result['nid'])
+        retrieved = r.json()
+        self.assertEqual(retrieved['field_link_api']['und'][0]['url'], ia_uri)
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
