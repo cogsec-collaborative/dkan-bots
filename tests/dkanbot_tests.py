592709ba269e3cf102c6f36b2dac47af69cec9e9
Grant Dobbe <grant@dobbe.us>
2020-04-06 11:06:21 -0700

    - refactored unit tests so I could use assertRegex - renamed dkanbot to libs - fixed a typo in the dkanbot lib

--- tests/dkanbot_tests.py
+++ tests/dkanbot_tests.py
@@ -1,15 +1,20 @@
-import os, unittest
-from dkan.client import DatasetAPI
+import unittest
+from libs import dkanbot
 
-# test internet archive submit
-def test_archive_submit():
-    pass
+class TestDKANBot(unittest.TestCase):
+    # test internet archive submit
+    def test_archive_submit(self):
+        test_uri = 'https://www.example.com'
+        result = dkanbot.upload_to_archive(test_uri)
+        self.assertRegex(result, 'https:\/\/web\.archive\.org\/web\/[\d]+\/https:\/\/www\.example\.com$')
 
-# test get node_id
-def test_node_id():
-    pass
+    # test get node_id
+    def test_node_id(self):
+        pass
 
-# test posting a resource to a dataset
-def test_resource_attach():
-    pass
-    
+    # test posting a resource to a dataset
+    def test_resource_attach(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
